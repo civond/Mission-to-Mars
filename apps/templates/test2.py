0c13a182288bc885f1047c94187fc18111a6e1bb
Dorian Yeh <56494806+civond@users.noreply.github.com>
2021-12-05 23:41:12 -0800

    Delete test2.py

--- apps/templates/test2.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://localhost:27017/test')
-db = client.test
-#my dummy database is myDatabase.
-coll1 = db.coll1 #selecting the coll1 in myDatabase
-for document in coll1.find():
-    print (document)
